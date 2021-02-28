a78380048f20f82f59a1a993722c8653bdf44cbf
hyperflow <hyperflow@kakao.com>
2021-03-01 03:02:08 +0900

    feat(App): 페이지들 Router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Switch } from 'react-router-dom';
 import GlobalStyle from 'components/base/GlobalStyle';
 import MainPage from 'pages/MainPage';
 import WebtoonPage from 'pages/WebtoonPage';
+import AboutPage from 'pages/AboutPage';
+import LoginPage from 'pages/LoginPage';
+import RegisterPage from 'pages/RegisterPage';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +13,9 @@ const App: React.FC = () => {
       <GlobalStyle />
       <Switch>
         <Route path="/" component={MainPage} exact />
+        <Route path="/about" component={AboutPage} exact />
+        <Route path="/login" component={LoginPage} exact />
+        <Route path="/register" component={RegisterPage} exact />
         <Route path="/webtoon/:id" component={WebtoonPage} />
       </Switch>
     </>
