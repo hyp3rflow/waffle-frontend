683cd4992bb8477a66fe2a676eba8155e979dcb1
hyperflow <hyperflow@kakao.com>
2021-03-01 02:59:46 +0900

    feat(ColorUtils): 이쁜 오렌지 베이스 블랙 추가

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -3,6 +3,7 @@ export const ColorPalette = {
   BRAND_BASE: '#ff8d3f',
   BRAND_ORANGE: '#ff7c24',
   BRAND_ACCENT: '#ffb37f',
+  BRAND_BLACK: '#2d1200',
   WHITE: '#ffffff',
   BLACK: '#222222',
   DARK_GRAY: '#212529',
